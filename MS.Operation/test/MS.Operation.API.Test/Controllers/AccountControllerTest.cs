50f34b0e4b2804e496dbded38b88d9f8fce2ddea
Quyk Mendonça <osilmar@outlook.com>
2019-03-24 20:09:18 -0300

    Implementa teste nos controllers

---
+++ MS.Operation/test/MS.Operation.API.Test/Controllers/AccountControllerTest.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MS.Operation.API.Controllers;
+using MS.Operation.Application.Interfaces;
+using MS.Operation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MS.Operation.API.Test.Controllers
+{
+    public class AccountControllerTest
+    {
+        private readonly Mock<IAccountService> _accountService;
+        private readonly AccountController _accountController;
+
+        public AccountControllerTest()
+        {
+            _accountService = new Mock<IAccountService>();
+            _accountController = new AccountController(_accountService.Object);
+        }
+
+        [Fact]
+        public void GetOkObjectResult()
+        {
+            var accounts = new List<Account>
+            {
+                new Account
+                {
+                    Number = 3064,
+                    Balance = 150.60,
+                    Limit = true
+                },
+                new Account
+                {
+                    Number = 6828,
+                    Balance = 170.96,
+                    Limit = false
+                }
+            };
+            _accountService.Setup(x => x.Get()).Returns(accounts);
+
+            var callback = _accountController.Get();
+
+            Assert.IsAssignableFrom<OkObjectResult>(callback);
+        }
+
+        [Fact]
+        public void GetNoContentResult()
+        {
+            _accountService.Setup(x => x.Get()).Returns(new List<Account>());
+
+            var callback = _accountController.Get();
+
+            Assert.IsAssignableFrom<NoContentResult>(callback);
+        }
+
+        [Fact]
+        public void GetBadRequestResult()
+        {
+            _accountService.Setup(x => x.Get()).Throws<Exception>();
+
+            var callback = _accountController.Get();
+
+            Assert.IsAssignableFrom<BadRequestResult>(callback);
+        }
+
+        [Fact]
+        public void GetByNumberAccountOkObjectResult()
+        {
+            var account = new Account
+            {
+                Number = 3064,
+                Balance = 150.60,
+                Limit = true
+            };
+
+            _accountService.Setup(x => x.GetByNumber(It.IsAny<Account>())).Returns(account);
+
+            var callback = _accountController.GetByNumber(3064);
+
+            Assert.IsAssignableFrom<OkObjectResult>(callback);
+        }
+
+        [Fact]
+        public void GetAccountNotFoundResult()
+        {
+            _accountService.Setup(x => x.GetByNumber(It.IsAny<Account>()));
+
+            var callback = _accountController.GetByNumber(3064);
+
+            Assert.IsAssignableFrom<NotFoundResult>(callback);
+        }
+
+        [Fact]
+        public void GetAccountBadRequestResult()
+        {
+            _accountService.Setup(x => x.GetByNumber(It.IsAny<Account>())).Throws<Exception>();
+
+            var callback = _accountController.GetByNumber(3064);
+
+            Assert.IsAssignableFrom<BadRequestResult>(callback);
+        }
+    }
+}
---
+++ MS.Operation/test/MS.Operation.API.Test/Controllers/TransferControllerTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MS.Operation.API.Controllers;
+using MS.Operation.Application.Interfaces;
+using MS.Operation.Domain.Entities;
+using MS.Operation.Domain.Enuns;
+using System;
+using Xunit;
+
+namespace MS.Operation.API.Test.Controllers
+{
+    public class TransferControllerTest
+    {
+        private readonly Mock<ITransferService> _transferService;
+        private readonly TransferController _transferController;
+        private readonly Transfer _transfer;
+
+        public TransferControllerTest()
+        {
+            _transferService = new Mock<ITransferService>();
+            _transferController = new TransferController(_transferService.Object);
+            _transfer = new Transfer
+            {
+                Origin = new Account { Number = 1124 },
+                Destination = new Account { Number = 4785 },
+                Value = 150.00
+            };
+        }
+
+        [Fact]
+        public void TransferConflictObjectResult()
+        {
+            _transfer.Origin = null;
+            _transferService.Setup(x => x.Transfer(_transfer)).Throws<Exception>();
+
+            var callback = _transferController.Post(_transfer);
+
+            Assert.IsAssignableFrom<ConflictObjectResult>(callback);
+        }
+
+        [Fact]
+        public void TransferBadRequest()
+        {
+            _transferService.Setup(x => x.Transfer(_transfer)).Throws<Exception>();
+
+            var callback = _transferController.Post(_transfer);
+
+            Assert.IsAssignableFrom<BadRequestResult>(callback);
+        }
+
+        [Fact]
+        public void TransferCreatedResult()
+        {
+            _transferService.Setup(x => x.Transfer(_transfer)).Returns(OperationStatus.Success);
+
+            var callback = _transferController.Post(_transfer);
+
+            Assert.IsAssignableFrom<CreatedResult>(callback);
+        }
+
+        [Fact]
+        public void TransferBadRequestObjectResult()
+        {
+            _transferService.Setup(x => x.Transfer(_transfer)).Returns(OperationStatus.NoBalance);
+
+            var callback = _transferController.Post(_transfer);
+
+            Assert.IsAssignableFrom<BadRequestObjectResult>(callback);
+        }
+    }
+}
