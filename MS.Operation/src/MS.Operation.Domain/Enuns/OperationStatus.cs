4d0d811b122ec4e2ac87798d0e2973d35fdb8adc
Quyk Mendonça <osilmar@outlook.com>
2019-03-24 02:58:10 -0300

    Adiciona enuns de operações

---
+++ MS.Operation/src/MS.Operation.Domain/Enuns/OperationStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace MS.Operation.Domain.Enuns
+{
+    public enum OperationStatus
+    {
+        [Description("Saldo insuficiente")]
+        NoBalance,
+        [Description("Conta de origem inválida")]
+        InvalidOriginAccount,
+        [Description("Conta de destino inválida")]
+        InvalidDestinationAccount,
+        [Description("Dados inválido")]
+        InvalidData,
+        [Description("Sucesso")]
+        Success,
+        [Description("Tente novamente mais tarde")]
+        TryAgainLater
+    }
+}
---
+++ MS.Operation/src/MS.Operation.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MS.Operation.Domain.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string Description(this Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var attributes = (DescriptionAttribute[]) fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            if (attributes != null && attributes.Length > 0)
+            {
+                return attributes[0].Description;
+            }
+
+            return value.ToString();
+        }
+    }
+}
